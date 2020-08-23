df8da171d15617463b14910f076b4733ba756752
Cristian Ninja <cristianferrarys@gmail.com>
2020-08-23 02:22:45 -0300

    Inicio notitest

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,20 +3,84 @@ import { Platform } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
+/// PUSH NOTIFI
+import { Push, PushObject, PushOptions } from '@ionic-native/push';
+
+
+
 import { HomePage } from '../pages/home/home';
+import { Pagina2Page } from '../pages/pagina2/pagina2';
+
 @Component({
   templateUrl: 'app.html'
 })
 export class MyApp {
-  rootPage:any = HomePage;
+  rootPage:any = Pagina2Page;
+  modulo:any ;
+  constructor(public platform: Platform, 
+    statusBar: StatusBar, 
+    splashScreen: SplashScreen,
+    private push: Push,
 
-  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
-    platform.ready().then(() => {
+  ) {
+    this.platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
       statusBar.styleDefault();
       splashScreen.hide();
+      
+      this.pushLLamado();
     });
+   // this.rootPage = Pagina2Page ;
+
+    
+    /*
+   this.platform.pause.subscribe(() => {
+    console.log('pause');
+   this.rootPage = HomePage;
+   });
+   this.platform.resume.subscribe(() => {
+    this.rootPage = Pagina2Page;
+    console.log('resume');
+   });
+   */
   }
+
+pushLLamado(){
+  const options: PushOptions = {
+    android: {
+      senderID: '187613377333',
+      sound: 'true',
+      vibrate: 'true',
+      
+
+    },
+    ios: {
+        alert: 'true',
+        badge: true,
+        sound: 'false'
+    }
+ };
+ 
+ const pushObject: PushObject = this.push.init(options);
+
+  pushObject.on('notification').subscribe((notification: any) => {
+    console.log('Received a notification');
+    console.log( notification);
+    ///this.modulo = notification.additionalData.Modulo;
+    //this.modulo = notification.additionalData.foreground;
+    alert(notification.additionalData.Modulo);
+  });
+ 
+  pushObject.on('registration').subscribe((registration: any) => {
+    console.log('Device registered', registration);
+  });
+  pushObject.on('error').subscribe(error => console.error('Error with Push plugin', error));
+
+
+}
+
+
+
 }
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,20 @@ import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
 
+///// PUSH
+import { Push } from '../../node_modules/@ionic-native/push';
+
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
+import { Pagina2Page } from '../pages/pagina2/pagina2';
+
+
 
 @NgModule({
   declarations: [
     MyApp,
-    HomePage
+    HomePage,
+    Pagina2Page
   ],
   imports: [
     BrowserModule,
@@ -19,11 +26,13 @@ import { HomePage } from '../pages/home/home';
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    HomePage
+    HomePage,
+    Pagina2Page
   ],
   providers: [
     StatusBar,
     SplashScreen,
+    Push,
     {provide: ErrorHandler, useClass: IonicErrorHandler}
   ]
 })
